8138c525242b513e784b6d523a025a8b39e6d21d
Gerard <48215770+geds133@users.noreply.github.com>
2020-04-28 17:22:22 +0100

    Delete pandas_read.py

--- pandas_read.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from matplotlib import image
-
-birds = pd.read_csv('bird_labs.csv', low_memory=False)
-attributes = pd.read_csv('attributes.csv', low_memory=False)
-certainties = pd.read_csv('certainties.csv', low_memory=False)
-parts = pd.read_csv('parts.csv', low_memory=False)
-class_labs_continuous = pd.read_csv('class_labs_continuous.csv', low_memory=False)
