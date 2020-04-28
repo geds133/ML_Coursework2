182628745a9161dd9c5fa217eb2d617d7a1fdcc9
Gerard <48215770+geds133@users.noreply.github.com>
2020-04-28 17:17:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -205,7 +205,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalisation import BatchNormalisation
-from keras.metrics import categorical_accuracy
 import matplotlib.pyplot as plt
 
 
